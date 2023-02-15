8147c332ba3ce25531b88d371b094f611e15adeb
bhaskarkh <bhaskarkh01@gmail.com>
2023-02-15 16:54:09 +0530

    anchor-escrow challenge done

--- Cluster-2/Code Challenge/solana-escrow-anchor/programs/solana-escrow-anchor/src/lib.rs
+++ Cluster-2/Code Challenge/solana-escrow-anchor/programs/solana-escrow-anchor/src/lib.rs
@@ -1,8 +1,8 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::system_program;
-use anchor_spl::token::{self, CloseAccount, SetAuthority, TokenAccount, Transfer};
+use anchor_spl::token::{self, CloseAccount, SetAuthority, TokenAccount, Transfer, Token};
 
-declare_id!("ECh7FQHy1hDxkiYjPVi8tYhmZ2oHE1zJqsyxbP4vS3nd");
+declare_id!("2aSLZWUCoTMwxCkJYCR4VgS48bT3FngEYXqffXfbccwz");
 
 #[program]
 pub mod solana_escrow_anchor {
@@ -10,8 +10,10 @@ pub mod solana_escrow_anchor {
     use super::*;
 
     const ESCROW_PDA_SEED: &[u8] = b"escrow";
-
-    pub fn initialize(ctx: Context<Initialize>, amount: u64) -> ProgramResult {
+    
+    // Changed from ProgramResult to Result<()> because from v0.22.0 you're supposed 
+    // to just use Result instead of ProgramResult
+    pub fn initialize(ctx: Context<Initialize>, amount: u64) -> Result<()> {
         // Store data in escrow account
         let escrow_account = &mut ctx.accounts.escrow_account;
         escrow_account.is_initialized = true;
@@ -24,11 +26,30 @@ pub mod solana_escrow_anchor {
         let (pda, _bump_seed) = Pubkey::find_program_address(&[ESCROW_PDA_SEED], ctx.program_id);
         token::set_authority(ctx.accounts.into(), AuthorityType::AccountOwner, Some(pda))?;
 
+        let clock = Clock::get()?;
+        escrow_account.unlock_time = clock.slot + 100;
+        escrow_account.time_out = &escrow_account.unlock_time + 1000;      
+        
         Ok(())
     }
 
-    pub fn exchange(ctx: Context<Exchange>, amount_expected_by_taker: u64) -> ProgramResult {
+    // Changed from ProgramResult to Result<()> because from v0.22.0 you're supposed 
+    // to just use Result instead of ProgramResult
+    pub fn exchange(ctx: Context<Exchange>, amount_expected_by_taker: u64) -> Result<()> {
         let escrow_account = &ctx.accounts.escrow_account;
+
+        if amount_expected_by_taker != ctx.accounts.pdas_temp_token_account.amount {
+            return Err(ErrorCode::ExpectedAmountMismatch.into());
+        }
+
+        let current_slot = Clock::get()?.slot;
+
+        if current_slot < escrow_account.unlock_time {
+            return Err(ErrorCode::EscrowUnlockTime.into());
+        }
+        if current_slot > escrow_account.time_out {
+            return Err(ErrorCode::EscrowTimeout.into());
+        }
 
         // Ensure that expected and deposited amount match
         if amount_expected_by_taker != ctx.accounts.pdas_temp_token_account.amount {
@@ -54,6 +75,17 @@ pub mod solana_escrow_anchor {
 
         Ok(())
     }
+
+    pub fn reset_time_lock(ctx: Context<ResetTimeLock>) -> Result<()> {
+        let escrow_account = &mut ctx.accounts.escrow_account;
+
+        let current_slot = Clock::get()?.slot;
+        escrow_account.unlock_time = current_slot + 100;
+        escrow_account.time_out = escrow_account.unlock_time + 1000;
+
+        Ok(())
+    }
+
 }
 
 #[derive(Accounts)]
@@ -62,20 +94,45 @@ pub struct Initialize<'info> {
     pub initializer: Signer<'info>,
     #[account(mut)]
     pub temp_token_account: Account<'info, TokenAccount>,
-    #[account(
-        constraint = *token_to_receive_account.to_account_info().owner == spl_token::id() @ ProgramError::IncorrectProgramId
+    #[account(owner = spl_token::id() @ ProgramError::IncorrectProgramId
     )]
     pub token_to_receive_account: Account<'info, TokenAccount>,
+    #[account(init, payer = initializer, space = Escrow::LEN)]
+    pub escrow_account: Account<'info, Escrow>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>, // needed for init escrow_init
+}
+
+#[derive(Accounts)]
+pub struct ResetTimeLock<'info> {
+    #[account(mut)]
+    pub initializer: Signer<'info>,
     #[account(
-        init, payer = initializer, space = Escrow::LEN,
-        constraint = !escrow_account.is_initialized @ ProgramError::AccountAlreadyInitialized
+        mut,
+        constraint = escrow_account.initializer_pubkey == initializer.key()
+    )]                  
+    pub escrow_account: Account<'info, Escrow>,
+}
+
+#[derive(Accounts)]
+pub struct Cancel<'info> {
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    #[account(mut)]
+    pub pda_token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub token_account_authority: AccountInfo<'info>,
+    #[account(
+        mut,
+        constraint = escrow_account.initializer_pubkey == initializer.key(),
+        constraint = escrow_account.temp_token_account_pubkey == pda_token_account.key(),
+        close = initializer
     )]
     pub escrow_account: Account<'info, Escrow>,
     #[account(address = spl_token::id())]
-    pub token_program: AccountInfo<'info>,
-    #[account(address = system_program::ID)]
-    pub system_program: AccountInfo<'info>, // needed for init escrow_init
+    pub token_program: AccountInfo<'info>
 }
+
 
 #[derive(Accounts)]
 pub struct Exchange<'info> {
@@ -87,8 +144,9 @@ pub struct Exchange<'info> {
     pub takers_token_to_receive_account: Account<'info, TokenAccount>,
     #[account(mut)]
     pub pdas_temp_token_account: Account<'info, TokenAccount>,
+    /// CHECK: we are just passing it to close in this so no checks required
     #[account(mut)]
-    pub initializers_main_account: AccountInfo<'info>,
+    pub initializers_main_account: UncheckedAccount<'info>,
     #[account(mut)]
     pub initializers_token_to_receive_account: Account<'info, TokenAccount>,
     #[account(mut, close = initializers_main_account,
@@ -97,9 +155,9 @@ pub struct Exchange<'info> {
         constraint = escrow_account.initializer_token_to_receive_account_pubkey == *initializers_token_to_receive_account.to_account_info().key @ ProgramError::InvalidAccountData,
     )]
     pub escrow_account: Box<Account<'info, Escrow>>,
-    #[account(address = spl_token::id())]
-    pub token_program: AccountInfo<'info>,
-    pub pda_account: AccountInfo<'info>,
+    pub token_program: Program<'info, Token>,
+    /// CHECK: we are not performing operations on pda account in this transaction
+    pub pda_account: UncheckedAccount<'info>,
 }
 
 #[account]
@@ -109,6 +167,8 @@ pub struct Escrow {
     pub temp_token_account_pubkey: Pubkey,
     pub initializer_token_to_receive_account_pubkey: Pubkey,
     pub expected_amount: u64,
+    pub unlock_time: u64,
+    pub time_out: u64,
 }
 
 const DISCRIMINATOR_LENGTH: usize = 8;
@@ -134,10 +194,14 @@ impl<'info> From<&mut Initialize<'info>> for CpiContext<'_, '_, '_, 'info, SetAu
     }
 }
 
-#[error]
+#[error_code]
 pub enum ErrorCode {
     #[msg("Amount expected by taker does not match the deposited amount of intitializer.")]
     ExpectedAmountMismatch,
+    #[msg("Current slot is less than unlock time. Escrow isn't initialized yet.")]
+    EscrowUnlockTime,
+    #[msg("Current slot is greater than timeout time")]
+    EscrowTimeout,
 }
 
 impl<'info> Exchange<'info> {
@@ -155,7 +219,7 @@ impl<'info> Exchange<'info> {
         let cpi_accounts = Transfer {
             from: self.pdas_temp_token_account.to_account_info().clone(),
             to: self.takers_token_to_receive_account.to_account_info().clone(),
-            authority: self.pda_account.clone(),
+            authority: self.pda_account.to_account_info().clone(),
         };
         let cpi_program = self.token_program.to_account_info();
         CpiContext::new(cpi_program, cpi_accounts)
@@ -164,8 +228,8 @@ impl<'info> Exchange<'info> {
     fn into_close_temp_token_context(&self) -> CpiContext<'_, '_, '_, 'info, CloseAccount<'info>> {
         let cpi_accounts = CloseAccount {
             account: self.pdas_temp_token_account.to_account_info().clone(),
-            destination: self.initializers_main_account.clone(),
-            authority: self.pda_account.clone(),
+            destination: self.initializers_main_account.to_account_info().clone(),
+            authority: self.pda_account.to_account_info().clone(),
         };
         let cpi_program = self.token_program.to_account_info();
         CpiContext::new(cpi_program, cpi_accounts)

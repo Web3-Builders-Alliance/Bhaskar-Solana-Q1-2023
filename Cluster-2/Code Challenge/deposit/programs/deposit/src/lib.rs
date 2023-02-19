1f0d532197e3904eecbc81b419e1195549b913f4
bhaskarkh <bhaskarkh01@gmail.com>
2023-02-20 01:07:06 +0530

    deposit code challenge & init code journal

--- Cluster-2/Code Challenge/deposit/programs/deposit/src/lib.rs
+++ Cluster-2/Code Challenge/deposit/programs/deposit/src/lib.rs
@@ -1,15 +1,181 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("4w6FdDYH1uBrPh4DSY6xwU7rT85bDYeWrX9xRCCVaoLX");
 
 #[program]
 pub mod deposit {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let deposit_account = &mut ctx.accounts.vault;
+        deposit_account.owner = ctx.accounts.initializer.key();
+        deposit_account.balance = 0;
+        deposit_account.bump = *ctx.bumps.get("vault").unwrap();
+        Ok(())
+    }
+
+    pub fn deposit (ctx: Context<Transfer>, deposit_amount: u64) -> Result<()> {
+        let deposit_account = &mut ctx.accounts.vault;
+        let owner_main_account = &mut ctx.accounts.owner;
+        require_keys_eq!(owner_main_account.key(),deposit_account.owner,DepError::Unauthorized);
+        require!(**owner_main_account.try_borrow_lamports()? >= deposit_amount, DepError::InsufficientFunds);
+    
+        let ix = anchor_lang::solana_program::system_instruction::transfer(
+            &owner_main_account.key(),
+            &deposit_account.key(),
+            deposit_amount,
+        );
+        
+        anchor_lang::solana_program::program::invoke(
+            &ix,
+            &[
+                owner_main_account.to_account_info(),
+                deposit_account.to_account_info(),
+            ],
+        )?;
+        deposit_account.balance += deposit_amount;
+
+        Ok(())
+    }
+    pub fn withdraw(ctx: Context<Withdraw>, withdraw_amount: u64) -> Result<()> {
+        let receiver = &mut ctx.accounts.owner;
+        let vault = &mut ctx.accounts.vault;
+        require_keys_eq!(receiver.key(),vault.owner,DepError::Unauthorized);
+        require!(withdraw_amount <= vault.balance, DepError::InsufficientFunds);
+
+        **vault.to_account_info().try_borrow_mut_lamports()? -= withdraw_amount;
+        **receiver.try_borrow_mut_lamports()? += withdraw_amount;
+        vault.balance -= withdraw_amount;
+
+        Ok(())
+    }
+    pub fn withdraw_og (ctx: Context<Transfer>, withdraw_amount: u64) -> Result<()> {
+        //doesn't work. 
+        let deposit_account = &mut ctx.accounts.vault;
+        let owner_main_account = &mut ctx.accounts.owner;
+        
+        require!(withdraw_amount <= deposit_account.balance, DepError::InsufficientFunds);
+
+        let ix = anchor_lang::solana_program::system_instruction::transfer(
+            &deposit_account.key(),
+            &owner_main_account.key(),
+            withdraw_amount,
+        );
+
+        let user_key = &owner_main_account.key();
+
+        let seeds = &[
+            "vault".as_ref(),
+            user_key.as_ref(),
+            &[deposit_account.bump],
+        ];
+        let signer = &[&seeds[..]];
+
+
+        anchor_lang::solana_program::program::invoke_signed(
+            &ix,
+            &[
+                deposit_account.to_account_info(),
+                owner_main_account.to_account_info()
+            ],
+            signer
+        )?;
+
+        deposit_account.balance -= withdraw_amount;
+
+        Ok(())
+    }
+
+    pub fn close_account (ctx: Context<Close>) -> Result<()> {
+
+        let deposit_account = &mut ctx.accounts.vault;
+        let owner_main_account = &mut ctx.accounts.owner;
+
+        let withdraw_amount: u64 = deposit_account.balance;
+
+        let ix = anchor_lang::solana_program::system_instruction::transfer(
+            &deposit_account.key(),
+            &owner_main_account.key(),
+            withdraw_amount,
+        );
+        
+        anchor_lang::solana_program::program::invoke(
+            &ix,
+            &[
+                deposit_account.to_account_info(),
+                owner_main_account.to_account_info()
+            ],
+        )?;
         Ok(())
     }
 }
 
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Transfer<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    #[account(
+        mut,
+        seeds=[b"vault".as_ref(), owner.key().as_ref()], 
+        bump
+    )]
+    pub vault: Account<'info, Vault>,
+    pub system_program: Program<'info, System>,
+}
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    #[account(mut)]
+    pub vault: Account<'info, Vault>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Close<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    #[account(
+        mut,
+        close = owner,
+        seeds=[b"vault".as_ref(), owner.key().as_ref()], 
+        bump
+    )]
+    pub vault: Account<'info, Vault>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    #[account(
+        init, 
+        payer = initializer, 
+        space = Vault::LEN,
+        seeds=[b"vault".as_ref(), initializer.key().as_ref()], 
+        bump
+    )]
+    pub vault: Account<'info, Vault>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Vault {
+    owner: Pubkey,
+    bump: u8,
+    balance: u64
+}
+
+impl Vault {
+    const LEN: usize = 8 + 32 + 1 + 8;
+}
+
+#[error_code]
+pub enum DepError {
+    #[msg("You don't have sufficient fund.")]
+    InsufficientFunds,
+    #[msg("Not authorized to perform this action.")]
+    Unauthorized,
+}
--- Cluster-2/Code Challenge/deposit/tests/deposit.ts
+++ Cluster-2/Code Challenge/deposit/tests/deposit.ts
@@ -1,16 +1,153 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { Deposit } from "../target/types/deposit";
+import { Keypair, PublicKey, Connection, SystemProgram, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { assert } from "chai";
+import { BN } from "bn.js";
 
-describe("deposit", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+describe("Deposit program test suite", async () => {
+      // Configure the client to use the local cluster.
+      anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.Deposit as Program<Deposit>;
+      const program = anchor.workspace.Deposit as Program<Deposit>;
+      const connection: Connection = program.provider.connection;
+      const user = Keypair.generate();
+        
+      // Fetch Vault PDA
+      const [vaultPda, vaultBump] = await PublicKey.findProgramAddressSync(
+        [Buffer.from("vault"), user.publicKey.toBuffer()],
+        program.programId
+      );
+before(async()=>{
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+      // Request an airdrop of 2 Sol to the user's account.
+      let airdropBlock = await connection.getLatestBlockhash('finalized');
+      try {
+        const airdrop = await connection.requestAirdrop(user.publicKey, 2 * LAMPORTS_PER_SOL);
+        await connection.confirmTransaction({
+          signature: airdrop,
+          blockhash: airdropBlock.blockhash,
+          lastValidBlockHeight: airdropBlock.lastValidBlockHeight
+        });
+      } catch (error) {
+        console.error("Error while requesting airdrop:", error);
+      }
+
+})
+  it("Creates a vault account", async () => {
+
+    let latestBlockhash = await connection.getLatestBlockhash('finalized');
+    try {
+      const tx = await program.methods.initialize()
+        .accounts({
+          initializer: user.publicKey,
+          vault: vaultPda,
+          systemProgram: SystemProgram.programId
+        })
+        .signers([user])
+        .rpc();
+      await connection.confirmTransaction({
+        signature: tx,
+        blockhash: latestBlockhash.blockhash,
+        lastValidBlockHeight: latestBlockhash.lastValidBlockHeight
+      });
+      console.log(`--------------TX URL------------`);
+      console.log(`https://explorer.solana.com/tx/${tx}?cluster=custom&customUrl=http%3A%2F%2Flocalhost%3A8899`); 
+    } catch (error) {
+      console.error("Error while creating a vault account:", error);
+    }
+
+    // Fetch the vault account and make assertions.
+    let vault;
+    try {
+      vault = await program.account.vault.fetch(vaultPda);
+    } catch (error) {
+      console.error("Error while fetching the vault account:", error);
+    }
+   console.log(`-----------VAULT CONTENT---------`);
+    console.log('owner:   ',vault.owner.toBase58());
+    console.log('bump:    ',vault.bump);
+    console.log('balance: ',vault.balance.toNumber());
+
+    assert(vault.owner.toBase58() === user.publicKey.toBase58(), "The vault owner should be the user.");
+    assert(vaultBump == vault.bump, "Vault bump as expected.");
+    assert(vault.balance.toNumber()== 0, "Vault balance is 0.");
+  });
+  it("Deposits 1 SOL", async () => {
+
+    let latestBlockhash = await connection.getLatestBlockhash('finalized');
+    let depositAmt = new BN(LAMPORTS_PER_SOL);
+    try {
+      const tx = await program.methods.deposit(depositAmt)
+        .accounts({
+          owner: user.publicKey,
+          vault: vaultPda,
+          systemProgram: SystemProgram.programId
+        })
+        .signers([user])
+        .rpc();
+      await connection.confirmTransaction({
+        signature: tx,
+        blockhash: latestBlockhash.blockhash,
+        lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
+      },'finalized');
+      console.log(`--------------TX URL------------`);
+      console.log(`https://explorer.solana.com/tx/${tx}?cluster=custom&customUrl=http%3A%2F%2Flocalhost%3A8899`); 
+    } catch (error) {
+      console.error("Error making deposit:", error);
+    }
+
+    // Fetch the vault account and make assertions.
+    let vault;
+    try {
+      vault = await program.account.vault.fetch(vaultPda);
+    } catch (error) {
+      console.error("Error while fetching the vault account:", error);
+    }
+   console.log(`-----------VAULT CONTENT---------`);
+    console.log('owner:   ',vault.owner.toBase58());
+    console.log('bump:    ',vault.bump);
+    console.log('balance: ',vault.balance.toNumber());
+
+    assert(vault.balance.toNumber()== LAMPORTS_PER_SOL, "Vault balance is 1 SOL.");
+  });
+  it("Withdraws 0.5 SOL", async () => {
+
+    let latestBlockhash = await connection.getLatestBlockhash('finalized');
+    let withdrawAmt = new BN(0.5*LAMPORTS_PER_SOL);
+    try {
+      const tx = await program.methods.withdraw(withdrawAmt)
+        .accounts({
+          owner: user.publicKey,
+          vault: vaultPda,
+          systemProgram: SystemProgram.programId
+        })
+        .signers([user])
+        .rpc();
+      await connection.confirmTransaction({
+        signature: tx,
+        blockhash: latestBlockhash.blockhash,
+        lastValidBlockHeight: latestBlockhash.lastValidBlockHeight
+      });
+      console.log(`--------------TX URL------------`);
+      console.log(`https://explorer.solana.com/tx/${tx}?cluster=custom&customUrl=http%3A%2F%2Flocalhost%3A8899`); 
+    } catch (error) {
+      console.error("Error making withdraw:");
+    }
+
+    // Fetch the vault account and make assertions.
+    let vault;
+    try {
+      vault = await program.account.vault.fetch(vaultPda);
+    } catch (error) {
+      console.error("Error while fetching the vault account:", error);
+    }
+    console.log(`-----------VAULT CONTENT---------`);
+    console.log('owner:   ',vault.owner.toBase58());
+    console.log('bump:    ',vault.bump);
+    console.log('balance: ',vault.balance.toNumber());
+
+    assert(vault.balance.toNumber()== 0.5*LAMPORTS_PER_SOL, "Vault balance is 0.5 SOL.");
+
   });
 });
